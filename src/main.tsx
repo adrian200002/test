06ff15e8b8d04dc64e123ec275b117567c0e9a38
Adrian <adrian.krause.ak@gmail.com>
2024-10-14 02:11:41 +0200

    changed to hash router

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./styles/fonts.css";
 import "./styles/reset.css";
 import App from "./App.tsx";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Navigate,
-} from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import ErrorPage from "./pages/ErrorPage.tsx";
 import HomePage from "./pages/HomePage.tsx";
 import WorldPage from "./pages/WorldPage.tsx";
@@ -15,42 +11,20 @@ import ToolsPage from "./pages/ToolsPage.tsx";
 import InteriorPage from "./pages/InteriorPage.tsx";
 import PewPage from "./pages/PewPage.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <Navigate to="/home" replace />,
-      },
-      {
-        path: "/home",
-        element: <HomePage />,
-      },
-      {
-        path: "/world-editor",
-        element: <WorldPage />,
-      },
-      {
-        path: "/tools",
-        element: <ToolsPage />,
-      },
-      {
-        path: "/interior",
-        element: <InteriorPage />,
-      },
-      {
-        path: "/pew",
-        element: <PewPage />,
-      },
-    ],
-  },
-]);
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/world-editor" element={<WorldPage />} />
+          <Route path="/tools" element={<ToolsPage />} />
+          <Route path="/interior" element={<InteriorPage />} />
+          <Route path="/pew" element={<PewPage />} />
+        </Route>
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </HashRouter>
   </StrictMode>
 );
